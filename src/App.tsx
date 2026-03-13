97853c05bb4fb9915e31bf1c2aadfb5b4e203e5d
DumitruN <dima.nirca00@gmail.com>
2026-03-13 18:01:29 +0200

    fix basename with env var

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/atelier-four-portfolio">
+      <BrowserRouter basename={import.meta.env.BASE_URL.replace(/\/+$/, "")}>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/portfolio" element={<Portfolio />} />
